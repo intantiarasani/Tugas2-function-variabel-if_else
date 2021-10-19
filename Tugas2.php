0edcbac12cd00178b0378eb0cda7b702b485da16
intantiarasani <72596819+intantiarasani@users.noreply.github.com>
2021-10-19 13:20:25 +0700

    Add files via upload

---
+++ Tugas2.php
@@ -0,0 +1,23 @@
+<?php
+//Function 
+function perkenalan(){
+	echo "Nama : Intan Tiara Sani <br/>";
+	echo "Prodi : Sitem Informasi <br/>";
+	echo "NIM : 20510015 <br>";
+}
+perkenalan();
+
+echo "<hr>";
+
+// Variabel dan if else
+$intan = "cantik";
+if ($intan == "cantik")
+{
+	echo "kalimat tersebut benar";
+}	
+  else
+{
+	echo "kalimat tersebut salah";
+} 
+
+?>
